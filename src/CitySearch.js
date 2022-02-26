76649c252472ef1e562117283ccd4dce558f1b4e
Ismail <vinux181@gmail.com>
2022-02-26 20:22:26 +0100

    removing onBlur

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -37,7 +37,6 @@ class CitySearch extends Component {
           value={this.state.query}
           onChange={this.handleInputChanged}
           onFocus={() => {this.setState({ showSuggestions: true }) }}
-          onBlur={() => {this.setState({ showSuggestions: false }) }}
         />
         <ul className='suggestions' style={this.state.showSuggestions ? {}: { display: 'none' }}>
           {this.state.suggestions.map((suggestion) => (
