cbe054cf5574397887dea7953183e27a8fc299cc
Ismail <vinux181@gmail.com>
2022-02-28 15:44:34 +0100

    4.7 adding alerts

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
   state = {
     query: '',
     suggestions: [],
-    showSuggestions: undefined
+    showSuggestions: undefined,
+    infoText: '',
   }
 
   handleInputChanged = (event) => {
@@ -12,16 +14,24 @@ class CitySearch extends Component {
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText: 'We can not find the city you are looking for. Please try another city',
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText: ''
+      });
+    }
   };
 
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion,
-      showSuggestions: false
+      showSuggestions: false,
     });
 
     this.props.updateEvents(suggestion);
@@ -30,15 +40,16 @@ class CitySearch extends Component {
   render() {
     return (
       <div className='CitySearch'>
-        <h2>Choose the nearest City</h2>
+        <InfoAlert text={this.state.infoText} />
         <input
           type='text'
           className='city'
+          placeholder='Search for a city'
           value={this.state.query}
           onChange={this.handleInputChanged}
           onFocus={() => {this.setState({ showSuggestions: true }) }}
         />
-        <ul className='suggestions' style={this.state.showSuggestions ? {}: { display: 'none' }}>
+        <ul className='suggestions' style={this.state.showSuggestions ? {} : { display: 'none' }}>
           {this.state.suggestions.map((suggestion) => (
             <li
               key={suggestion}
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,25 +1,35 @@
 import React, { Component } from "react";
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
-    numberOfEvents: 32
+    numberOfEvents: 32,
+    infoText: '',
   }
 
   handleInputChanged = (event) => {
     const value = event.target.value;
+    if (value < 1 || value > 32) {
+      return this.setState({
+        infoText: 'Select a number between 1 - 32',
+        numberOfEvents: '',
+      });
+    } else {
     this.setState({
       numberOfEvents: value,
+      infoText: '',
     });
     this.props.updateEvents(null, event.target.value);
-  }
+    }
+  };
 
   render() {
     return <div className="numberOfEvents">
+      <ErrorAlert text={this.state.infoText} />
       <h3>Number of events</h3>
       <input
         type="number"
         className="events-number"
-        min='1' max='32'
         value={this.state.numberOfEvents}
         onChange={this.handleInputChanged}
       />
