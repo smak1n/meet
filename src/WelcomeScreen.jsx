cd27b73e59f3c32262ee4e5505237c015703488b
Ismail <vinux181@gmail.com>
2022-03-02 17:18:58 +0100

    update styling

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -1,39 +1,52 @@
 import React from "react";
 import './WelcomeScreen.css';
+import { Container, Col, Row } from 'react-bootstrap';
 
 function WelcomeScreen(props) {
   return props.showWelcomeScreen ?
   (
   <div className="WelcomeScreen">
-    <h1>Welcome to the Meet app</h1>
-    <h4>
-      Log in to see upcoming events around the world for
-      full-stack
-      developers
-    </h4>
-    <div className="button_cont" align="center">
-      <div class="google-btn">
-        <div class="google-icon-wrapper">
-          <img
-            class="google-icon"
-            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-            alt="Google sign-in"
-          />
+    <Container fluid>
+    <Row>
+      <Col>
+        <h1>Welcome to the Meet app</h1>
+        <h4>
+          Log in to see upcoming events around the world for
+          full-stack
+          developers
+        </h4>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <div className="button_cont" align="center">
+          <div class="google-btn">
+            <div class="google-icon-wrapper">
+              <img
+                class="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="Google sign-in"
+              />
+            </div>
+            <button onClick={() => { props.getAccessToken() }}
+            rel="nofollow noopener"
+            class="btn-text"
+            >
+            <b>Sign in with google</b>
+            </button>
+          </div>
         </div>
-        <button onClick={() => { props.getAccessToken() }}
+      </Col>
+    </Row>
+    <Row>
+      <a 
+        href="https://smak1n.github.io/meet/privacy.html"
         rel="nofollow noopener"
-        class="btn-text"
-        >
-        <b>Sign in with google</b>
-        </button>
-      </div>
-    </div>
-    <a 
-      href="https://smak1n.github.io/meet/privacy.html"
-      rel="nofollow noopener"
-    >
-    Privacy policy
-    </a>
+      >
+      Privacy policy
+      </a>
+    </Row>
+    </Container>
   </div>
   ) : null
 }
